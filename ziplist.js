7c6d8d05120de80834de61a516ae615fbda8f50d
acnarciso <anarciso@middlebury.com>
2018-09-17 21:38:06 -1000

    Completed HW

---
+++ ziplist.js
@@ -0,0 +1,18 @@
+//14:39
+
+function zipList(list1, list2) {
+  const newList = [];
+  for (let i = 0; i < list1.length; i++) {
+    newList.push(list1[i]);
+    newList.push(list2[i]);
+  }
+  return newList;
+}
+
+console.log(zipList(['a', 'b', 'c'], [1, 2, 3]));
+
+function zipListTheSimpleWay(list1, list2) {
+  return _.flatten(_.zip(list1, list2));
+}
+
+console.log(zipListTheSimpleWay(['a', 'b', 'c'], [1, 2, 3]));
